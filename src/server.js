5f56a9bec65bdd380b85a54cebf5251b2be511dc
julianaibiapina <julianibiapina@alu.ufc.br>
2020-06-11 15:44:24 -0300

    template engine ok

--- src/server.js
+++ src/server.js
@@ -4,15 +4,22 @@ const server = express();
 // configura a pasta public para que esteja disponível no diretório raiz
 server.use(express.static("public"));
 
-// ------CONFIGURAR CAMINHOS DA APLICAÇÃO
+// configurar nunjucks
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
 
-// req: requisição
-// res: resposta
+// ------ configurar caminhos no servidor
 
 server.get("/", (req, res) =>{
-    // __dirname é uma variável já criada pelo node, referenciando nosso diretório
-    res.sendFile(__dirname + "/views/index.html");
+    return res.render("index.html")
 } );
+
+
+
+
 
 // ligar o servidor na porta 3000
 server.listen(3000);
