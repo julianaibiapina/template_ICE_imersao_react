608b62eaa4990feaab505845cc363867e915c9a1
julianaibiapina <julianibiapina@alu.ufc.br>
2020-08-21 18:06:47 -0300

    Revert "ICE layout init"
    This reverts commit e5748777e7014f9a6b09d817a586e140e77728e1.

--- src/componentes/Footer/styles.js
+++ src/componentes/Footer/styles.js
@@ -1,52 +1,14 @@
-/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
+
 export const FooterBase = styled.footer`
-  background-color: var(--primary);
-  /* border-top: 2px solid #FF9900; */
-  color: var(--white);
-  padding: 48px 5%;
-
-  display: flex;
-  justify-content: space-between;
-  
-
-  div{
-    flex: 1;
-  }
-  h3{
-    width: 80%;
-    border-bottom: 2px solid #FF9900;
-  }
-
-  a{
-    display: flex;
-    align-items: center;
-    /* color: var(--title-color); */
-    font-weight: 700;
-    margin-bottom: 8px;
-  }
-  ul{
-    margin: 0;
-    padding: 10px;
-  }
-  .icon_social{   
-   
-    margin-right: 12px;
-    height: 36px;
-    width: 36;
-
-  }
-
-  .map { 
-        width: 100%; 
-        height: 200px; 
-        background-color: grey; 
-      } 
+ background-color: var(--black);
+ border-top: 2px solid var(--primary);
+ color: var(--white);
+ padding: 36px 16px;
+ text-align: center;
 
   @media(max-width: 800px){
     margin-bottom: 50px;
   }
-
-
 `;
--- src/componentes/Menu/style.js
+++ src/componentes/Menu/style.js
@@ -1,27 +1,22 @@
 import styled from 'styled-components';
 
 export const LogoImage = styled.img`
-    /* max-width: 168px; */
-    max-height: 48px;
+    max-width: 168px;
 
     @media (max-width: 800px){
-        max-width: 48px;
+        max-width: 105px;
     }
 `;
 
 export const MenuWrapper = styled.nav`
     
     width: 100%;
-    height: 78px;
+    height: 94px;
 
-    /* position: fixed;
-    z-index: 1; */
-    /* top: 0;
-    left: 0; */
-
-    
-    padding-top: 6px;
-    padding-bottom: 5px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
     padding-left: 5%;
     padding-right: 5%;
 
@@ -29,11 +24,11 @@ export const MenuWrapper = styled.nav`
     justify-content: space-between;
     align-items: center;
 
-    background-color: var(--primary);
-    border-bottom: 2px solid #14CCB9;
+    background-color: var(--black);
+    border-bottom: 2px solid var(--primary);
 
     @media(max-width: 800px) {
-        height: 58px;
+        height: 40px;
         justify-content: center;
         /* z-index: 4; */
     }
